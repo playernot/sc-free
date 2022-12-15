c370e639905018a5506d091e273e5c5def6d05c2
playernot <118193682+playernot@users.noreply.github.com>
2022-12-15 11:48:44 +0700

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,24 +1,24 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-let tag = `https://wa.me/6282214729677`
+let tag = `wa.me/6285780036817`
   let mentionedJid = [m.sender]
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
-let suka = `Saya Putra`
+let suka = `Saya Xyro`
 global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': suka, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${suka},;;;\nFN:${suka},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
 
 let str = `*Hai Ngab*`
-let isi =`I'm Kaylabotz Yang Di Buat Gak Niat Oleh Putra Karena Males Recode Jadi Pake Aja Apa Yang Ada Yah.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
+let isi =`I'm LuluBotz Yang Di Buat Gak Niat Oleh Xyro Karena Males Recode Jadi Pake Aja Apa Yang Ada Yah.\nKalau Ada Yg Error Bisa Langsung Report Ke Owner.\n${tag} (Owner)`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.send3ButtonImg(m.chat, `https://telegra.ph/file/116c1a217394d408415c9.jpg`, str, isi, 'Menu', '.command', 'Owner', '.owner', 'Credit', '.credit', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: 'https://instagram.com/l4c1f2r',
+    mediaUrl: 'https://instagram.com/xyrostore_real',
     mediaType: 2, 
     description: urlnya,
     title: "Follow Ig Gw",
     body: wm,
     thumbnail: thumb,
-    sourceUrl: 'https://instagram.com/l4c1f2r'
+    sourceUrl: 'https://instagram.com/xyrostore_real'
      }}
   })
           }
