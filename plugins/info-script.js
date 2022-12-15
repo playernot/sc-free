f1edb7d7bb6699d8750fe96cd0150e0a22110fb0
playernot <118193682+playernot@users.noreply.github.com>
2022-12-15 12:42:24 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch')
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/Lucife-Putra* (Follow banh)
-sc ini: wa.me/6282214729677
+let str = `*https://github.com/playernot* (Follow banh)
+sc ini: wa.me/6285780036817
 support terus bang thanks`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
@@ -11,7 +11,7 @@ conn.sendButtonDoc(m.chat, str, wm,'O W N E R','.owner', m, { contextInfo: { ext
     mediaUrl: "https://Instagram.com/l4c1f2r",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'KaylaBotz-MD',
+    title: 'LuluBotz',
     body: wm,
     thumbnail: thumb,
     sourceUrl: urlnya
